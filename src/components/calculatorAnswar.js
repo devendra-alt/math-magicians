6774fdcd1f5129120b138f077021a3ebe746c562
devendra-alt <devendramulewa171@gmail.com>
2023-07-19 14:10:23 +0530

    Added CalculaterAnswer Component for display

--- src/components/calculatorAnswar.js
+++ src/components/calculatorAnswar.js
@@ -1,8 +1,15 @@
-const CalculatorAnswer = () => (
+import PropTypes from 'prop-types';
+
+const CalculatorAnswer = ({ result, next, operation }) => (
   <td colSpan={4} id="calculator-answer-el">
-    0
+    <p>
+      <span>{result}</span>
+      <span>{operation}</span>
+      <span>{next}</span>
+    </p>
   </td>
 );
+
 CalculatorAnswer.propTypes = {
   result: PropTypes.string.isRequired,
   next: PropTypes.string.isRequired,
