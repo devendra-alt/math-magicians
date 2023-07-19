b29f0c8249246771a978752357714c3b4dd9bde1
devendra-alt <devendramulewa171@gmail.com>
2023-07-19 14:09:13 +0530

    Added setState result for calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,14 @@
+import { useState } from 'react';
 import CalculatorAnswer from './calculatorAnswar';
 import calculate from '../logic/calculate';
+import keyevents from '../logic/keyevents';
+
+export default function Calculator() {
+  const [result, setResult] = useState({});
+  function handleClick(event) {
+    setResult(calculate(result, event.target.innerText));
+  }
+
   document.body.addEventListener('keydown', (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -8,3 +17,55 @@ import calculate from '../logic/calculate';
       setResult(calculate(result, key));
     }
   });
+
+  return (
+    <article className="calculator-container">
+      <table>
+        <tbody>
+          <tr>
+            <CalculatorAnswer
+              result={result.total ? result.total : ''}
+              next={result.next ? result.next : ''}
+              operation={result.operation ? result.operation : ''}
+            />
+          </tr>
+          <tr>
+            <td onClick={handleClick}>AC</td>
+            <td onClick={handleClick}>+/-</td>
+            <td onClick={handleClick}>%</td>
+            <td onClick={handleClick}>&#247;</td>
+          </tr>
+          <tr>
+            <td onClick={handleClick}>7</td>
+            <td onClick={handleClick}>8</td>
+            <td onClick={handleClick}>9</td>
+            <td onClick={handleClick}>x</td>
+          </tr>
+          <tr>
+            <td onClick={handleClick}>4</td>
+            <td onClick={handleClick}>5</td>
+            <td onClick={handleClick}>6</td>
+            <td onClick={handleClick}>-</td>
+          </tr>
+          <tr>
+            <td onClick={handleClick}>1</td>
+            <td onClick={handleClick}>2</td>
+            <td onClick={handleClick}>3</td>
+            <td onClick={handleClick}>+</td>
+          </tr>
+          <tr>
+            <td
+              className="calculator-zero-el"
+              colSpan={2}
+              onClick={handleClick}
+            >
+              0
+            </td>
+            <td onClick={handleClick}>.</td>
+            <td onClick={handleClick}>=</td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  );
+}
