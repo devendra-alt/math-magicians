ef032f3d2c391f582d6a7d9c4a511e106fd26542
devendra-alt <devendramulewa171@gmail.com>
2023-07-20 22:57:09 +0530

    Use useEffect to fetch data from api-ninja endpoint

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,3 +1,22 @@
+  useEffect(() => {
+    const header = new Headers();
+    header.append('x-api-key', 'OVLcX2/3XpWrpr1fR1+FqA==HqgZvSKqav5X8B74');
+    setisLoading(true);
+    fetch('https://api.api-ninjas.com/v1/quotes?category=', {
+      method: 'GET',
+      headers: header,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setisLoading(false);
+        setQuote(data[0]);
+      })
+      .catch((e) => {
+        setisLoading(false);
+        setErrorMessage(`error fetching data ${e}`);
+      });
+  }, []);
+
   if (isLoading) return <h1 className="is-loading-msg utils">Loading....</h1>;
   if (errorMessage) {
     return <h1 className="is-error-msg utils">{errorMessage}</h1>;
