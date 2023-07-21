71df0e23a92c3c3aeeb0869ab1e999f349ccab3c
devendra-alt <devendramulewa171@gmail.com>
2023-07-21 09:51:38 +0530

    Add onClick event on reload button in quote.js

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 
+const header = new Headers();
+header.append('x-api-key', 'OVLcX2/3XpWrpr1fR1+FqA==HqgZvSKqav5X8B74');
+const URL = 'https://api.api-ninjas.com/v1/quotes?category=';
+
 export default function Quote() {
   const [quoteData, setQuote] = useState({});
   const [isLoading, setisLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  useEffect(() => {
-    const header = new Headers();
-    header.append('x-api-key', 'OVLcX2/3XpWrpr1fR1+FqA==HqgZvSKqav5X8B74');
+  const handlefetchApi = () => {
     setisLoading(true);
-    fetch('https://api.api-ninjas.com/v1/quotes?category=', {
+    fetch(URL, {
       method: 'GET',
       headers: header,
     })
@@ -22,7 +24,9 @@ export default function Quote() {
         setisLoading(false);
         setErrorMessage(`error fetching data ${e}`);
       });
-  }, []);
+  };
+
+  useEffect(() => handlefetchApi(), []);
 
   if (isLoading) return <h1 className="is-loading-msg utils">Loading....</h1>;
   if (errorMessage) {
@@ -32,8 +36,8 @@ export default function Quote() {
     <article className="quote-container">
       <p className="quote">{quoteData?.quote}</p>
       <p className="quote-author">{quoteData?.author}</p>
-      <button type="submit" className="quote-suffler">
-        .
+      <button type="submit" className="quote-suffler" onClick={handlefetchApi}>
+        <span className="quote-suffler-tooltip">reload</span>
       </button>
     </article>
   );
