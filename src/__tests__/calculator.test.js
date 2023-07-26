9c6aabc20b5d76e4c8283f9928db52cecf876fb5
devendra-alt <devendramulewa171@gmail.com>
2023-07-26 23:04:25 +0530

    Add tests for calculator in calculator.test.js

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,16 +1,65 @@
-import { Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import React from 'react';
 import Calculator from '../components/calculator.js';
-
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 describe('renders correctly', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(
-      <Router>
-
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
         <Calculator />
-      </Router>
-    ).toJSON();
+      </MemoryRouter>
+    );
+  });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <Calculator />
+        </MemoryRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('should have calculator have title on screen', async () => {
+    const titleNode = await screen.getByText("Let's do some math");
+    expect(titleNode).toHaveTextContent("Let's do some math");
+  });
+  it('should have calculator buttons on screen', () => {
+    const buttonNodes = screen.getAllByLabelText('calculator-btn');
+    expect(buttonNodes).toHaveLength(19);
+  });
+  it('should add running value (aka next)', () => {
+    const buttonNodes = screen.getAllByLabelText('calculator-btn');
+    let sevenNode = null;
+    let fiveNode = null;
+    buttonNodes.forEach((element) => {
+      if (element.textContent === '7') {
+        sevenNode = element;
+      }
+      if (element.textContent === '5') {
+        fiveNode = element;
+      }
+    });
+
+    sevenNode.innerText = '7';
+    fiveNode.innerText = '5';
+
+    fireEvent.click(sevenNode);
+    fireEvent.click(fiveNode);
+
+    const answer = screen.getByLabelText('calculator-answer-p-span-next');
+    expect(answer.textContent).toBe('75');
+  });
+  it('Backspace should work', async () => {
+    const answer_ = screen.getByLabelText('calculator-answer-p-span-next');
+    await userEvent.keyboard('{7}');
+    await userEvent.keyboard('{7}');
+    await userEvent.keyboard('{7}');
+    await userEvent.keyboard('{7}');
+    await userEvent.keyboard('{Backspace}');
+    expect(answer_.textContent).toBe('777');
   });
 });
