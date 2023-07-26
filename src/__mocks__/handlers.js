d48f255caee2572966db32ea496c3bd19ea592ae
devendra-alt <devendramulewa171@gmail.com>
2023-07-26 23:30:32 +0530

    Fix tyos in handlers.js

--- src/__mocks__/handlers.js
+++ src/__mocks__/handlers.js
@@ -1,19 +1,17 @@
 import { rest } from 'msw';
 
 const handlers = [
-  rest.get('https://api.api-ninjas.com/v1/quotes', (req, res, ctx) =>
-    res(
-      ctx.status(200),
-      ctx.json([
-        {
-          quote:
+  rest.get('https://api.api-ninjas.com/v1/quotes', (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json([
+      {
+        quote:
             'A great empire, like a great cake, is most easily diminished at the edges.',
-          author: 'Jean Piaget',
-          category: 'best',
-        },
-      ])
-    )
-  ),
+        author: 'Jean Piaget',
+        category: 'best',
+      },
+    ])
+  )),
 ];
 
 export default handlers;
