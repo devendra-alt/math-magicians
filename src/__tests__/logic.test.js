120fe00a30ee785780ce682a0dacdd17259348e7
devendra-alt <devendramulewa171@gmail.com>
2023-07-26 17:25:29 +0530

    Add Test calculator logic in logic.test.js

--- src/__tests__/logic.test.js
+++ src/__tests__/logic.test.js
@@ -1,3 +1,31 @@
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
+
+let obj = {
+  total: '450',
+  next: '50',
+  operation: '+',
+};
+
+describe('Test calculator logic', () => {
+  it('should add two values and give answer', () => {
+    const { total } = calculate(obj, '=');
+    expect(total).toStrictEqual('500');
+  });
+
+  it('should clear total', () => {
+    const { total } = calculate(obj, 'AC');
+    expect(total).toStrictEqual(null);
+  });
+
+  it('shoud return divide by zero', () => {
+    obj['total'] = '0';
+    obj['next'] = '0';
+    const { total } = calculate(obj, '%');
+    expect(total).toStrictEqual('0');
+  });
+});
+
 describe('Test operate logic', () => {
   it('add 2+4 ', () => {
     expect(operate('2', '4', '+')).toStrictEqual('6');
