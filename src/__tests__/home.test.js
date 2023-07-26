90c27b3acc48ad156c40e9ebf15306db910d8687
devendra-alt <devendramulewa171@gmail.com>
2023-07-26 23:05:24 +0530

    Add tests for home in home.test.js

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -1,6 +1,5 @@
 import renderer from 'react-test-renderer';
-import App from '../App';
-
+import App from '../App.js';
 describe('renders correctly', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
